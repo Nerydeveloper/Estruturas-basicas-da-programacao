37a458fb354ff8f8cc27f540eeaa06e058f97407
Adrian Nery <125079978+Nerydeveloper@users.noreply.github.com>
2023-04-07 11:09:49 -0300

    Add files via upload

---
+++ python/Estruturas/sequenciais.py
@@ -0,0 +1,11 @@
+print("olá,mundo!!!")
+print("Segue ai galera")
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("digite a segunda nota: "))
+n3 = float(input("Digite o terceira nota: "))
+n4 = float(input("Digite a quarta nota: "))
+
+media = (n1 + n2 + n3 + n4) /4
+
+print("Sua media foi: ", media)
